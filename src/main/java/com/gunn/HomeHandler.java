df430f0aa8054cbfa03142c959234691e4880f9f
mrgunn-bnds <danielgunn@bnds.cn>
2025-05-20 09:04:33 +0800

    Added a HomeHandler to handle the dynamic logic.  We can now have 50% chance of winning.

--- src/main/java/com/gunn/HomeHandler.java
+++ src/main/java/com/gunn/HomeHandler.java
@@ -8,14 +8,19 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 
-public class StaticFileHandler implements HttpHandler {
+public class HomeHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         //System.out.println(exchange.getRequestURI().getPath());
-        String filePath = exchange.getRequestURI().getPath();
-        filePath = "www/" + filePath;
+        String filePath = "www/index.html";
         File file = new File(filePath);
-        byte[] contents = Files.readAllBytes(file.toPath());
+        String text = Files.readString(file.toPath());
+        if (Math.random() > 0.5) {
+            text = text.replace("{{RESULT}}", "You lose!");
+        } else {
+            text = text.replace("{{RESULT}}", "You win!");
+        }
+        byte[] contents = text.getBytes();
 
         exchange.sendResponseHeaders(200, contents.length);
         OutputStream os = exchange.getResponseBody();
--- src/main/java/com/gunn/TeacherMonServer.java
+++ src/main/java/com/gunn/TeacherMonServer.java
@@ -8,7 +8,9 @@ import java.net.InetSocketAddress;
 public class TeacherMonServer {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(80),0);
-        server.createContext("/", new StaticFileHandler());
+        server.createContext("/", new HomeHandler());
+        server.createContext("/favicon.ico", new StaticFileHandler());
+        server.createContext("/gunn.png", new StaticFileHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
